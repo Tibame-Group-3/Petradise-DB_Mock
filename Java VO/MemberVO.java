9e739e0fd08a79787c0540bc630a76e86508b9d3
hsuramone <133873273+hsuramone@users.noreply.github.com>
2023-05-22 13:55:23 +0800

    Add VO
    Pet, Member VO

---
+++ Java VO/MemberVO.java
@@ -0,0 +1,75 @@
+
+public class MemberVO {
+	private Integer memID;
+	private String memName;
+	private String memAccount;
+	private String memPassword;
+	private Date memBirthday;
+	private String memPhone;
+	private String memEmail;
+	private String memAddress;
+	private Character memAccess;
+	private	Integer memBonus;
+	public Integer getMemID() {
+		return memID;
+	}
+	public void setMemID(Integer memID) {
+		this.memID = memID;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	public String getMemAccount() {
+		return memAccount;
+	}
+	public void setMemAccount(String memAccount) {
+		this.memAccount = memAccount;
+	}
+	public String getMemPassword() {
+		return memPassword;
+	}
+	public void setMemPassword(String memPassword) {
+		this.memPassword = memPassword;
+	}
+	public Date getMemBirthday() {
+		return memBirthday;
+	}
+	public void setMemBirthday(Date memBirthday) {
+		this.memBirthday = memBirthday;
+	}
+	public String getMemPhone() {
+		return memPhone;
+	}
+	public void setMemPhone(String memPhone) {
+		this.memPhone = memPhone;
+	}
+	public String getMemEmail() {
+		return memEmail;
+	}
+	public void setMemEmail(String memEmail) {
+		this.memEmail = memEmail;
+	}
+	public String getMemAddress() {
+		return memAddress;
+	}
+	public void setMemAddress(String memAddress) {
+		this.memAddress = memAddress;
+	}
+	public Character getMemAccess() {
+		return memAccess;
+	}
+	public void setMemAccess(Character memAccess) {
+		this.memAccess = memAccess;
+	}
+	public Integer getMemBonus() {
+		return memBonus;
+	}
+	public void setMemBonus(Integer memBonus) {
+		this.memBonus = memBonus;
+	}
+	
+	
+}
---
+++ Java VO/PetVO.java
@@ -0,0 +1,58 @@
+
+public class PetVO {
+
+	private Integer petID;
+	private Integer memID;
+	private String petName;
+	private String petType;
+	private Character petSize;
+	private Character petStatus;
+
+	public Integer getPetID() {
+		return petID;
+	}
+
+	public void setPetID(Integer petID) {
+		this.petID = petID;
+	}
+
+	public Integer getMemID() {
+		return memID;
+	}
+
+	public void setMemID(Integer memID) {
+		this.memID = memID;
+	}
+
+	public String getPetName() {
+		return petName;
+	}
+
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
+
+	public String getPetType() {
+		return petType;
+	}
+
+	public void setPetType(String petType) {
+		this.petType = petType;
+	}
+
+	public Character getPetSize() {
+		return petSize;
+	}
+
+	public void setPetSize(Character petSize) {
+		this.petSize = petSize;
+	}
+
+	public Character getPetStatus() {
+		return petStatus;
+	}
+
+	public void setPetStatus(Character petStatus) {
+		this.petStatus = petStatus;
+	}
+}

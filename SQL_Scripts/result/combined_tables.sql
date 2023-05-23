f70ae66003b499197da0c71ff9e19830927cc0e2
hsuramone <133873273+hsuramone@users.noreply.github.com>
2023-05-23 19:43:45 +0800

    remove space in folder name

--- SQL_Scripts/result/combined_tables.sql
+++ SQL_Scripts/result/combined_tables.sql
@@ -196,6 +196,22 @@ ALTER TABLE pet ADD(
 ALTER TABLE bonus ADD(
     CONSTRAINT fk_bonus_member FOREIGN KEY (mem_id)  REFERENCES member (mem_id)
 );
+
+
+/* administrator_access */
+alter table administrator_access add(
+	constraint fk_admin_id foreign key(admin_id) references administrator(admin_id),
+    constraint fk_function_id foreign key(function_id) references access_function(function_id)
+);
+
+/* room_order */
+alter table room_order add(
+	constraint fk_mem_id foreign key(mem_id) references member(mem_id),
+	constraint fk_room_type_id foreign key(room_type_id) references room_type(room_type_id),
+    constraint fk_room_id foreign key(room_id) references room(room_id),
+	constraint fk_pet_id foreign key(pet_id) references pet(pet_id)
+);
+
 -- fav_list--
 ALTER TABLE fav_list ADD(
     CONSTRAINT fk_fav_list_member FOREIGN KEY (mem_id) REFERENCES member (mem_id),

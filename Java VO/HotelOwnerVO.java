d8e356718bb74f081f2805bd681dae0079f1914d
peipei30 <133873451+peipei30@users.noreply.github.com>
2023-05-23 17:24:01 +0800

    java vo

---
+++ Java VO/HotelOwnerVO.java
@@ -0,0 +1,149 @@
+package com.hotel.model;
+
+public class HotelOwnerVO implements java.io.Serializable {
+	private int hotelId;
+	private String hotelName;
+	private String hotelAddress;
+	private String hotelStatus;
+	private String hotelLicId;
+	private byte[] hotelLicPic;
+	private Integer reviewScorePeople;
+	private Integer reviewScoreTotal;
+	private String ownerAccount;
+	private String ownerPassword;
+	private String ownerName;
+	private String ownerId;
+	private String ownerBank;
+	private String ownerPhone;
+	private String ownerEmail;
+	private String ownerAccess;
+
+	public int getHotelId() {
+		return hotelId;
+	}
+
+	public void setHotelId(int hotelId) {
+		this.hotelId = hotelId;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public String getHotelAddress() {
+		return hotelAddress;
+	}
+
+	public void setHotelAddress(String hotelAddress) {
+		this.hotelAddress = hotelAddress;
+	}
+
+	public String getHotelStatus() {
+		return hotelStatus;
+	}
+
+	public void setHotelStatus(String hotelStatus) {
+		this.hotelStatus = hotelStatus;
+	}
+
+	public String getHotelLicId() {
+		return hotelLicId;
+	}
+
+	public void setHotelLicId(String hotelLicId) {
+		this.hotelLicId = hotelLicId;
+	}
+
+	public byte[] getHotelLicPic() {
+		return hotelLicPic;
+	}
+
+	public void setHotelLicPic(byte[] hotelLicPic) {
+		this.hotelLicPic = hotelLicPic;
+	}
+
+	public Integer getReviewScorePeople() {
+		return reviewScorePeople;
+	}
+
+	public void setReviewScorePeople(Integer reviewScorePeople) {
+		this.reviewScorePeople = reviewScorePeople;
+	}
+
+	public Integer getReviewScoreTotal() {
+		return reviewScoreTotal;
+	}
+
+	public void setReviewScoreTotal(Integer reviewScoreTotal) {
+		this.reviewScoreTotal = reviewScoreTotal;
+	}
+
+	public String getOwnerAccount() {
+		return ownerAccount;
+	}
+
+	public void setOwnerAccount(String ownerAccount) {
+		this.ownerAccount = ownerAccount;
+	}
+
+	public String getOwnerPassword() {
+		return ownerPassword;
+	}
+
+	public void setOwnerPassword(String ownerPassword) {
+		this.ownerPassword = ownerPassword;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public String getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(String ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public String getOwnerBank() {
+		return ownerBank;
+	}
+
+	public void setOwnerBank(String ownerBank) {
+		this.ownerBank = ownerBank;
+	}
+
+	public String getOwnerPhone() {
+		return ownerPhone;
+	}
+
+	public void setOwnerPhone(String ownerPhone) {
+		this.ownerPhone = ownerPhone;
+	}
+
+	public String getOwnerEmail() {
+		return ownerEmail;
+	}
+
+	public void setOwnerEmail(String ownerEmail) {
+		this.ownerEmail = ownerEmail;
+	}
+
+	public String getOwnerAccess() {
+		return ownerAccess;
+	}
+
+	public void setOwnerAccess(String ownerAccess) {
+		this.ownerAccess = ownerAccess;
+	}
+
+}

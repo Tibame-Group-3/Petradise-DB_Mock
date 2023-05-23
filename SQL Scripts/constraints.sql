7b90c7aa37abcef3f71b52e14c8cbaaeddbd52f4
peipei30 <133873451+peipei30@users.noreply.github.com>
2023-05-23 17:30:50 +0800

    Update constraints.sql

--- SQL Scripts/constraints.sql
+++ SQL Scripts/constraints.sql
@@ -9,3 +9,24 @@ ALTER TABLE pet ADD(
 ALTER TABLE bonus ADD(
     CONSTRAINT fk_bonus_member FOREIGN KEY (mem_id)  REFERENCES member (mem_id)
 );
+-- fav_list--
+ALTER TABLE fav_list ADD(
+    CONSTRAINT fk_fav_list_member FOREIGN KEY (mem_id) REFERENCES member (mem_id),
+    CONSTRAINT fk_fav_list_room_type FOREIGN KEY (room_type_id) REFERENCES room_type (room_type_id)
+);
+-- room_type--
+ALTER TABLE room_type ADD(
+  
+    CONSTRAINT fk_room_type_hotel_owner FOREIGN KEY (hotel_id) REFERENCES hotel_owner (hotel_id)
+);
+-- room--
+ALTER TABLE room ADD(
+  CONSTRAINT fk_room_room_type FOREIGN KEY (room_type_id) REFERENCES room_type (room_type_id),
+  CONSTRAINT fk_room_pet FOREIGN KEY (pet_id) REFERENCES pet (pet_id)
+);
+
+-- room_pic--
+ALTER TABLE room ADD(
+    CONSTRAINT fk_room_pic_room_type FOREIGN KEY (room_type_id) REFERENCES room_type (room_type_id)
+);
+

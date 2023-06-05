4fe818bef12dbec78b90ccdcc41773b72840905c
HsuRamone <hsuramone@gmail.com>
2023-06-05 16:48:02 +0800

    feat: add Pet mock data, update combined_tables.sql

--- SQL_Scripts/Pet.sql
+++ SQL_Scripts/Pet.sql
@@ -6,3 +6,16 @@ CREATE TABLE `pet` (
     pet_size CHAR(1),
     pet_status CHAR(1) NOT NULL DEFAULT '0'
 );
+
+INSERT INTO `pet` (mem_id, pet_name, pet_type, pet_size)
+VALUES
+    (1, 'Fluffy', 'Cat', 'S'),
+    (2, 'Buddy', 'Dog', 'M'),
+    (3, 'Charlie', 'Dog', 'L'),
+    (4, 'Whiskers', 'Cat', 'M'),
+    (5, 'Max', 'Dog', 'L'),
+    (6, 'Coco', 'Cat', 'S'),
+    (7, 'Rocky', 'Dog', 'M'),
+    (8, 'Luna', 'Cat', 'S'),
+    (9, 'Bailey', 'Dog', 'L'),
+    (10, 'Milo', 'Cat', 'M');

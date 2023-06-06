07be6a21f377083eae01f1fa7d6fdd8998a86eaa
peipei30 <133873451+peipei30@users.noreply.github.com>
2023-06-06 23:40:38 +0800

    hotel-旅館收藏清單pk改為自動編號 insert假資料

--- SQL_Scripts/fav-list.sql
+++ SQL_Scripts/fav-list.sql
@@ -1,5 +1,8 @@
 CREATE TABLE `fav_list` (
-  `fav_list_id` INT NOT NULL PRIMARY KEY,
+  `fav_list_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `mem_id` INT NOT NULL,
   `room_type_id` INT NOT NULL
    );
+   INSERT INTO fav_list (mem_id, room_type_id) VALUES 
+(1, 5), (1, 6), (2, 7), (2, 8), (3, 9),
+(3, 10), (4, 11), (4, 12), (5, 13), (5, 14);

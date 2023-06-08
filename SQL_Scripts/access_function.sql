227473dc1af8b1f263bc715f27aa29339847b0df
siajs <ksh70070@gmail.com>
2023-06-08 17:34:43 +0800

    modify sql files

--- SQL_Scripts/access_function.sql
+++ SQL_Scripts/access_function.sql
@@ -1,11 +1,11 @@
 create table access_function(
 	function_id int primary key not null,
-    function_name varchar(20) not null
+    function_name char(1) not null
 );
 
 insert into access_function (function_id, function_name)
 values
-(1, '旅館'),
-(2, '收容'),
-(3, '商城'),
-(4, '員工');
+(1, '0'),
+(2, '1'),
+(3, '2'),
+(4, '3');

ea1dc7c472f0abeee7c95725a4f0a4ae4eb5c637
siajs <ksh70070@gmail.com>
2023-06-12 16:31:34 +0800

    mock data

--- SQL_Scripts/result/combined_tables.sql
+++ SQL_Scripts/result/combined_tables.sql
@@ -41,18 +41,18 @@ create table admin(
     admin_status char(1) not null default '0'
 );
 
-INSERT INTO admin (admin_id, admin_name, admin_account, admin_password, admin_phone, admin_address, admin_email, admin_title)
+INSERT INTO admin (admin_id, admin_name, admin_account, admin_password, admin_email, admin_title)
 VALUES
-(1, 'John Doe', 'johndoe', 'pass123', '1234567890', '123 Main St, City', 'johndoe@example.com', '主管'),
-(2, 'Jane Smith', 'janesmith', 'password1', '9876543210', '456 Elm St, Town', 'janesmith@example.com', '主管'),
-(3, 'David Johnson', 'davidjohnson', 'secret123', '5551234567', '789 Oak Ave, Village', 'davidjohnson@example.com', '正職'),
-(4, 'Emily Brown', 'emilybrown', 'abc123', '1112223333', '321 Pine Rd, City', 'emilybrown@example.com', '正職'),
-(5, 'Michael Davis', 'michaeldavis', 'qwerty', '4445556666', '654 Cedar Ln, Town', 'michaeldavis@example.com', '正職'),
-(6, 'Olivia Wilson', 'oliviawilson', 'adminpass', '7778889999', '987 Spruce Dr, City', 'oliviawilson@example.com', '正職'),
-(7, 'William Taylor', 'williamtaylor', 'password123', '2223334444', '159 Birch St, Village', 'williamtaylor@example.com', '工讀'),
-(8, 'Sophia Anderson', 'sophiaanderson', 'testpass', '8889990000', '852 Maple Ave, Town', 'sophiaanderson@example.com', '工讀'),
-(9, 'James Martin', 'jamesmartin', '12345678', '6667778888', '753 Oakwood Dr, City', 'jamesmartin@example.com', '工讀'),
-(10, 'Isabella Thompson', 'isabellathompson', 'pass1234', '3334445555', '456 Elm St, Village', 'isabellathompson@example.com', '正職');
+(1, 'John Doe', 'johndoe', 'pass123', 'johndoe@example.com', '0'),
+(2, 'Jane Smith', 'janesmith', 'password1', 'janesmith@example.com', '0'),
+(3, 'David Johnson', 'davidjohnson', 'davidjohnson@example.com', '0'),
+(4, 'Emily Brown', 'emilybrown', 'abc123', 'emilybrown@example.com', '0'),
+(5, 'Michael Davis', 'michaeldavis', 'qwerty', 'michaeldavis@example.com', '1'),
+(6, 'Olivia Wilson', 'oliviawilson', 'adminpass', 'oliviawilson@example.com', '1'),
+(7, 'William Taylor', 'williamtaylor', 'password123', 'williamtaylor@example.com', '1'),
+(8, 'Sophia Anderson', 'sophiaanderson', 'testpass', 'sophiaanderson@example.com', '2'),
+(9, 'James Martin', 'jamesmartin', '12345678', 'jamesmartin@example.com', '2'),
+(10, 'Isabella Thompson', 'isabellathompson', 'pass1234', 'isabellathompson@example.com', '2');
 
 
 
@@ -208,7 +208,6 @@ CREATE TABLE `hotel_owner` (
 );
 
 
-<<<<<<< HEAD
 INSERT INTO `hotel_owner` (
   `hotel_name`,
   `hotel_address`,
@@ -245,10 +244,7 @@ INSERT INTO `hotel_owner` (
 
 
 
-/* CREATE_TABLE_status_date */
-=======
 /* CREATE_TABLE_lostpetarticle */
->>>>>>> 27
 
 CREATE TABLE `lost_pet_article` (
 	article_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,

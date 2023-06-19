1f45af0b0f165b39e258851283d7e287bbe3555f
peipei30 <133873451+peipei30@users.noreply.github.com>
2023-06-19 20:38:44 +0800

    房間表格欄位pet_id改為pet_name fk刪除pet_id

--- SQL_Scripts/room.sql
+++ SQL_Scripts/room.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `room` (
   `room_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `room_type_id` INT NOT NULL,
-   `pet_id`INT ,
+   `pet_name` VARCHAR(30),
   `room_name` VARCHAR(30),
   `room_sale_status` CHAR(1) NOT NULL DEFAULT '0',
   `room_status` CHAR(1) NOT NULL DEFAULT '0'
   );
- INSERT INTO room (room_type_id, room_name, pet_id,room_sale_status, room_status) VALUES 
+ INSERT INTO room (room_type_id, room_name, pet_name,room_sale_status, room_status) VALUES 
 (5, '房間A', NULL, '0', '0'), (5, '房間B', NULL,'0', '0'), (5, '房間C',NULL, '0', '0'), 
 (6, '房間A', NULL,'0', '0'), (6, '房間B',NULL, '0', '0'),  
 (7, '房間A', NULL,'0', '0'), (7, '房間B', NULL,'0', '0'), (7, '房間C',NULL, '0', '0'), 

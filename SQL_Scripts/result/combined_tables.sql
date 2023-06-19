ed617c3096ea97be5a110df63537f4cc84255e28
HsuRamone <hsuramone@gmail.com>
2023-06-19 21:13:43 +0800

    feat: update combined_tables.sql

--- SQL_Scripts/result/combined_tables.sql
+++ SQL_Scripts/result/combined_tables.sql
@@ -477,7 +477,13 @@ create table `sale_project` (
 
 /* CREATE_TABLE_room_pic */
 
-SELECT * FROM gp3.animal;
+CREATE TABLE `room_pic` (
+  `room_pic_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+  `room_type_id` INT NOT NULL,
+  `room_pic` LONGBLOB 
+ );
+
+
 
 
 /* CREATE_TABLE_promiselist */
@@ -525,28 +531,30 @@ create table `product` (
 CREATE TABLE `room` (
   `room_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `room_type_id` INT NOT NULL,
-   `pet_id`INT ,
+   `pet_name` VARCHAR(30),
   `room_name` VARCHAR(30),
   `room_sale_status` CHAR(1) NOT NULL DEFAULT '0',
   `room_status` CHAR(1) NOT NULL DEFAULT '0'
   );
- INSERT INTO room (room_type_id, pet_id,room_name, room_sale_status, room_status) VALUES 
-(5, NULL, '房間A', '0', '0'), (5, NULL,'房間B','0', '0'), (5,NULL, '房間C', '0', '0'), 
-(6,  NULL,'房間A', '0', '0'), (6,NULL, '房間B', '0', '0'),  
-(7, NULL,'房間A', '0', '0'), (7,NULL, '房間B', '0', '0'), (7,NULL, '房間C', '0', '0'), 
-(8,NULL, '房間A','0', '0'), (8, NULL,'房間B','0', '0'), (8,NULL, '房間C', '0', '0'), 
-(9,NULL, '房間A','0', '0'), (9, NULL,'房間B','0', '0'), (9,NULL, '房間C', '0', '0'), 
-(10,NULL, '房間A', '0', '0'), (10,NULL, '房間B', '0', '0'),
-(11,NULL, '房間A', '0', '0'), (11, NULL,'房間B', '0', '0'),
-(12, NULL,'房間A', '0', '0'), (12,NULL, '房間B', '0', '0'),
-(13, NULL,'房間A','0', '0'), (13,NULL, '房間B', '0', '0'),
-(14, NULL,'房間A', '0', '0'), (14, NULL,'房間B', '0', '0'),
-(15,NULL, '房間A', '0', '0'), (15,NULL, '房間B', '0', '0'),
-(16, NULL,'房間A', '0', '0'), (16, NULL,'房間B', '0', '0'),
-(17,NULL, '房間A', '0', '0'), (17,NULL, '房間B', '0', '0'),
-(18,NULL,'房間A', '0', '0'), (18,NULL, '房間B', '0', '0'),
-(19,NULL, '房間A','0', '0'), (19,NULL, '房間B', '0', '0');
- 
+
+ INSERT INTO room (room_type_id, room_name, pet_name,room_sale_status, room_status) VALUES 
+(5, '房間A', NULL, '0', '0'), (5, '房間B', NULL,'0', '0'), (5, '房間C',NULL, '0', '0'), 
+(6, '房間A', NULL,'0', '0'), (6, '房間B',NULL, '0', '0'),  
+(7, '房間A', NULL,'0', '0'), (7, '房間B', NULL,'0', '0'), (7, '房間C',NULL, '0', '0'), 
+(8, '房間A', NULL,'0', '0'), (8, '房間B',NULL, '0', '0'), (8, '房間C',NULL, '0', '0'), 
+(9, '房間A', NULL,'0', '0'), (9, '房間B',NULL, '0', '0'), (9, '房間C', NULL,'0', '0'), 
+(10, '房間A', NULL,'0', '0'), (10, '房間B',NULL, '0', '0'),
+(11, '房間A',NULL, '0', '0'), (11, '房間B',NULL, '0', '0'),
+(12, '房間A', NULL,'0', '0'), (12, '房間B',NULL, '0', '0'),
+(13, '房間A',NULL, '0', '0'), (13, '房間B',NULL, '0', '0'),
+(14, '房間A', NULL,'0', '0'), (14, '房間B',NULL, '0', '0'),
+(15, '房間A', NULL,'0', '0'), (15, '房間B',NULL, '0', '0'),
+(16, '房間A', NULL,'0', '0'), (16, '房間B', NULL,'0', '0'),
+(17, '房間A',NULL, '0', '0'), (17, '房間B',NULL, '0', '0'),
+(18, '房間A',NULL, '0', '0'), (18, '房間B',NULL, '0', '0'),
+(19, '房間A', NULL,'0', '0'), (19, '房間B',NULL, '0', '0');
+
+
 
 
 
@@ -715,9 +723,8 @@ ALTER TABLE room_type ADD(
 );
 -- room--
 ALTER TABLE room ADD(
-  CONSTRAINT fk_room_room_type FOREIGN KEY (room_type_id) REFERENCES room_type (room_type_id),
-	CONSTRAINT fk_room_pet FOREIGN KEY (pet_id) REFERENCES pet (pet_id)
-);
+  CONSTRAINT fk_room_room_type FOREIGN KEY (room_type_id) REFERENCES room_type (room_type_id)
+	);
 
 -- room_pic--
 ALTER TABLE room ADD(
